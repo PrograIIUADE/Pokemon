6263f0bf13d5e707f03abfeda3591f9ae932c49b
ofernandez22 <ofernandez22@gmail.com>
2016-11-14 21:35:29 -0300

    .

--- Pokedex/src/Test.java
+++ Pokedex/src/Test.java
@@ -3,9 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		PokedexTDA Pokedex= new Pokedex();
-		Pokedex.inicializar();
+		Pokedex.inicializar();// pokearbol =null
 		System.out.format("Se van a cargar los pokemones \n");
-	//	Pokedex.agregar(pokemon);
+		Pokemon pokemon= new Pokemon();
+		pokemon.tipo.Acero.getClass();
+	Pokedex.agregar(pokemon);
 		
 
 	}
